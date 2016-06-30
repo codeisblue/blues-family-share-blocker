700d8990b808fd4c90af40751525413be3547ed8
Lewis <LewisLector@users.noreply.github.com>
2016-06-30 10:35:08 +1000

    Update bfsb_config.lua
    Fixed some tabbing and a spelling mistake.

--- blues-family-shared-blocker/lua/bfsb_config.lua
+++ blues-family-shared-blocker/lua/bfsb_config.lua
@@ -5,12 +5,12 @@ BFSB = {}
 
 --Put in your (Get it here : https://steamcommunity.com/dev/apikey). If this is not set then it will not work.
 BFSB.STEAM_DEV_API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" 
-												 
---If this is set to true it will automaticaly kick any players that user family sharing (Not recommended as some are legit)
+	 
+--If this is set to true, it will automaticaly kick any players that user family sharing (Not recommended as some are legit)
 BFSB.KICK_SHARED_ACCOUNTS = false 
  
---If true this will auto kick any players that join who are family shared with an account that has already been banned from the server (Recommended)
+--If true, this will auto kick any players that join who are family shared with an account that has already been banned from the server (Recommended)
 BFSB.KICK_SHARED_ACCOUNTS_WITH_BANNED_OWNERS = true
 
---If true this will ban the family sharing owner when an account sharing withj it is banned (Recommended)
+--If true, this will ban the family sharing owner when an account sharing with it is banned (Recommended)
 BFSB.BAN_OWNING_ACCOUNT_ON_SHARED_BANNED = true
